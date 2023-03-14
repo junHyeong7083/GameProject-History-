d08e7e23eb1ff6aaff4199cefd4ab50bdfe7fa56
junHyeong7083 <127365389+junHyeong7083@users.noreply.github.com>
2023-03-15 00:00:26 +0900

    LogoScene
    로고랑 타이틀 페이드인 페이드아웃

---
+++ Assets/script/Logo/FadeInFadeOut.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public enum FadeState {  FadeIn = 0, FadeOut , FadeInOut, FadeLoop };
+
+public class FadeInFadeOut : MonoBehaviour
+{
+    [Header("설정한 값이 원하는 시간의 절반")]
+    [SerializeField]
+    private float fadeTime;
+    [SerializeField]
+    private Image image;
+    private FadeState fadeState;
+
+    public Image Logo;
+    bool isLogo = false;
+    public Image Title;
+    bool isTitle = false;   
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+
+        OnFade(FadeState.FadeInOut);
+    }
+
+    public void OnFade(FadeState state)
+    {
+        fadeState = state;
+        switch(fadeState)
+        {
+            case FadeState.FadeIn:
+                StartCoroutine(Fade(1, 0)); // FadeIn ,배경의 알파값이 1에서 0
+                break;
+            case FadeState.FadeOut:
+                StartCoroutine(Fade(0, 1)); // FadeOut 배경의 알파값이 0 에서 1
+                break;
+            case FadeState.FadeInOut: // 1회 반복
+            case FadeState.FadeLoop:
+                StartCoroutine(FadeInOut());
+                break;
+        }
+    }
+    int cnt = 1; // cnt = 1 logo , cnt =2 Title
+    bool NextScene = false;
+    public IEnumerator FadeInOut()
+    {
+        while(true)
+        {
+              yield return StartCoroutine(Fade(0, 1)); // fade out
+              yield return StartCoroutine(Fade(1, 0)); // fade in
+
+            if(fadeState == FadeState.FadeInOut && cnt == 2)
+            {
+                NextScene = true;
+                break;
+            }
+            cnt++;
+        }
+    }
+
+    private IEnumerator Fade(float start, float end)
+    {
+        float currentTime = 0f;
+        float percent = 0f;
+
+        while(percent < 1)
+        {
+            currentTime += Time.deltaTime;
+            percent = currentTime / fadeTime;
+
+            Color color = image.color;
+            color.a = Mathf.Lerp(start,end, percent);
+            image.color = color;
+
+            if(cnt == 1)
+            {
+                Color color1 = Logo.color;
+                color1.a = Mathf.Lerp(start, end, percent);
+                Logo.color = color1;
+            }
+            if(cnt == 2)
+            {
+                Color color2 = Title.color;
+                color2.a = Mathf.Lerp(start, end, percent);
+                Title.color = color2;
+            }
+
+
+
+            yield return null;
+        }
+    }
+
+    private void Update()
+    {
+        if(!NextScene)
+        {
+            // 씬매니저로 다음씬넘겨버리기
+        }
+    }
+}
