866c834082b60460ca9743e3954e839aee4d7ead
junHyeong7083 <127365389+junHyeong7083@users.noreply.github.com>
2023-03-20 22:01:30 +0900

    Move Player
    캐릭터움직임 어찌저찌 성공
    offset변수 : 공격 생섬범위 시작점관련됨

--- Assets/script/Player/PlayerController.cs
+++ Assets/script/Player/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -29,50 +30,61 @@ public class PlayerController : MonoBehaviour
 
     bool isAtk = false; // 공격중일때는 움직임x
     bool isRender = false;
+    bool isMoving = false;
     void Start()
     {
     }
-     Vector2 getTouchPosition(Vector2 touchPosition)
-    {
-        return GetComponent<Camera>().ScreenToWorldPoint(new Vector3(touchPosition.x, touchPosition.y, 0));
 
-    }
-   
+    int EndedFinger = 0;
     void Update()
     {
-       if (Input.touchCount > 0) // 첫 입력
+        if (Input.touchCount > 0) // 첫 입력
         {
             touch1 = FindTouchById(0);
             if (Input.GetTouch(0).phase == TouchPhase.Moved || Input.GetTouch(0).phase == TouchPhase.Began)
             {
-                if(!isAtk)
+                if (!isAtk)
                 {
-                  
-                    dir = (touch1.position - touch1.rawPosition).normalized;
-                    dirtxt.text = "x : " + dir.x + "y : " + dir.y;
 
+                    dir = (touch1.position - touch1.rawPosition).normalized / 3;
+                    dirtxt.text = "x : " + dir.x + "y : " + dir.y;
+                    isMoving = true;
                     Player.transform.Translate(dir * speed * Time.deltaTime);
                 }
             }
-            if (Input.touchCount > 1) // 2번째 입력
+            else if(Input.GetTouch(0).phase == TouchPhase.Stationary)
+            {
+                if(!isAtk)
+                {
+                    dir = new Vector2(0, 0);
+                    Player.transform.Translate(dir * speed * Time.deltaTime);
+                }
+            }
+ 
+            if (Input.touchCount >= 1) // 2번째 입력
             {
                 touch2 = FindTouchById(1);
                 if (Input.GetTouch(1).phase == TouchPhase.Moved)
                 {
                     isAtk = true;
-                    if(isAtk)
+                    isMoving = false;
+                    if (isAtk)
                     {
                         AtkRange = Camera.main.ScreenToWorldPoint(touch2.position);
                         AtkRange.z = 0f;
                         atktxt.text = "atkx : " + AtkRange.x + "atky : " + AtkRange.y;
 
+                        Vector3 dir = AtkRange.normalized;
+
                         isRender = true;
                         if (isRender)
                         {
+                            Vector3 offset = dir.normalized * 0.5f;
                             Vector3[] positions = new Vector3[lineRenderer.positionCount];
                             lineRenderer.GetPositions(positions);
-                            positions[0] = playerTransform.position;
-                            positions[1] = AtkRange;
+                            positions[0] = playerTransform.position + offset; // 플레이어 좌표
+                            positions[1] = AtkRange; // 공격 드래그 끝좌표
+                            lineRenderer.enabled = true;
                             lineRenderer.SetPositions(positions);
                         }
                     }
@@ -80,40 +92,36 @@ public class PlayerController : MonoBehaviour
                     // AtkRnage : 드래그시 좌표값 - 드래그할때마다 갱신됨
                     // LaserPoint랑 AtkRange값 이용해서 라인렌더러 사용하면삘
                 }
-                else if (Input.GetTouch(1).phase == TouchPhase.Ended) // 손을 뗏을때
+                else if (Input.GetTouch(1).phase == TouchPhase.Ended && Input.GetTouch(0).phase == TouchPhase.Ended) // 손을 뗏을때
                 {
+
                     isAtk = false;
-                    if (isRender)
+
+                    if (isRender) // 그렸던 라인을 지우는 과정
                     {
                         Vector3 touchWorldPosition = Camera.main.ScreenToWorldPoint(touch2.position);
                         Player.transform.position = new Vector2(touchWorldPosition.x, touchWorldPosition.y);
                         isRender = false;
-                     //   lineRenderer.enabled = false;
+                        lineRenderer.enabled = false;
                         lineRenderer.SetPositions(new Vector3[0]);
-                    } 
+                    }
                 }
 
+
             }
 
-            if(Input.GetTouch(0).phase == TouchPhase.Ended)
+            Touch FindTouchById(int id)
             {
-                lineRenderer.enabled = false;
-                lineRenderer.SetPositions(new Vector3[0]);
-            }
-        }
-      
-       Touch FindTouchById(int id)
-        {
-            for(int i = 0; i<Input.touchCount; ++i)
-            {
-                Touch t = Input.GetTouch(i);
-                if (t.fingerId == id) return t;
-            }
+                for (int i = 0; i < Input.touchCount; ++i)
+                {
+                    Touch t = Input.GetTouch(i);
+                    if (t.fingerId == id) return t;
+                }
 
-            return default (Touch);
+                return default(Touch);
+            }
         }
     }
-
 
 
 }
