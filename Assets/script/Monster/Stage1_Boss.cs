b2b3d4668519d84b4faa53737f37d3ea0a74c406
junHyeong7083 <127365389+junHyeong7083@users.noreply.github.com>
2023-03-22 23:51:01 +0900

    Move & Atk
    진짜 양손다떼는거 개줫같네

--- Assets/script/Monster/Stage1_Boss.cs
+++ Assets/script/Monster/Stage1_Boss.cs
@@ -6,10 +6,14 @@ using UnityEngine.UI;
 
 public class Stage1_Boss : MonoBehaviour
 {
-    public Image TestImage;
     Rigidbody2D rigidbody2D;
+
+
+    // ----------------- bool -----------------
+    bool isOverRab = false;
     void Start()
     {
+        // 보스 현재 포지션 0, 2
         rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
--- Assets/script/Player/PlayerController.cs
+++ Assets/script/Player/PlayerController.cs
@@ -16,10 +16,13 @@ public class PlayerController : MonoBehaviour
     [Header("TouchMoved")]
     Touch touch1; // 이동 터치
     Touch touch2; // 공격 터치
-    Vector2 dir; // 이동에 적용될 벡터
-
-    //-------------------공격관련-------------------
-    [Header("TouchAtk")]
+    Vector2 smoothedDirection;
+    Vector3 previousPosition;
+    Vector3 touchPosition;
+    Vector2 touch2prePoisition;
+    Vector3 touchWorldPosition;
+   //-------------------공격관련-------------------
+   [Header("TouchAtk")]
     public LineRenderer lineRenderer;
     public float lineDrawSpeed;
     Vector3 AtkRange; //
@@ -32,10 +35,12 @@ public class PlayerController : MonoBehaviour
     //---------------------bool--------------------------
     bool isAtk = false; // 공격중일때는 움직임x
     bool isRender = false;
-    bool isMoving = false;
     bool isRay = false;
+    bool isTouch = false;
+
     //---------------------Els--------------------------
     public Image TestImage;
+    public Text testtest;
     void Start()
     {
         lineCollider = Line.GetComponent<EdgeCollider2D>();
@@ -47,104 +52,121 @@ public class PlayerController : MonoBehaviour
         if (Input.touchCount > 0) // 첫 입력
         {
             touch1 = FindTouchById(0);
-            if (FindTouchById(0).phase == TouchPhase.Moved || FindTouchById(0).phase == TouchPhase.Began)
+            if (FindTouchById(0).phase == TouchPhase.Began)
+            {
+                previousPosition = touch1.position;
+                smoothedDirection = Vector2.zero;
+            }
+            else if (FindTouchById(0).phase == TouchPhase.Moved)
             {
                 if (!isAtk)
                 {
+                    touchPosition = touch1.position;
+                    Vector2 newDirection = (touchPosition - previousPosition).normalized;
+                    float distance = Vector2.Distance(touchPosition, previousPosition);
+                    float mindistance = 3f;
+                    float smoothFactor = 0.5f; // 입력 방향 보정 계수
+                    if (distance > mindistance)
+                    {
+                        smoothedDirection = Vector2.Lerp(smoothedDirection, newDirection, smoothFactor);
+                        Player.transform.Translate(smoothedDirection * speed * Time.deltaTime);
+                        previousPosition = touchPosition;
+                    }
 
-                    dir = (touch1.position - touch1.rawPosition).normalized / 3;
-                    // dirtxt.text = "x : " + dir.x + "y : " + dir.y; 테스트
-                    isMoving = true;
-                    Player.transform.Translate(dir * speed * Time.deltaTime);
                 }
             }
 
-            if (Input.touchCount >= 1) // 2번째 입력
+            else if (FindTouchById(0).phase == TouchPhase.Stationary)
             {
-                touch2 = FindTouchById(1);
-                if (Input.GetTouch(1).phase == TouchPhase.Moved)
+                Player.transform.Translate(Vector2.zero * speed * Time.deltaTime);
+                smoothedDirection = Vector2.zero; // 입력 방향 보정 초기화
+            }
+
+            touch2 = FindTouchById(1);
+            touch2prePoisition = FindTouchById(1).position;
+
+             if (FindTouchById(1).phase == TouchPhase.Moved)
+            {
+                isAtk = true;
+                if (isAtk)
                 {
-                    isAtk = true;
-                    isMoving = false;
-                    if (isAtk)
+                    AtkRange = Camera.main.ScreenToWorldPoint(touch2.position);
+                    AtkRange.z = 0f;
+                    // atktxt.text = "atkx : " + AtkRange.x + "atky : " + AtkRange.y; 테스트
+
+                    Vector3 dir = AtkRange.normalized;
+
+                    isRender = true;
+                    if (isRender)
                     {
-                        AtkRange = Camera.main.ScreenToWorldPoint(touch2.position);
-                        AtkRange.z = 0f;
-                        // atktxt.text = "atkx : " + AtkRange.x + "atky : " + AtkRange.y; 테스트
+                        // 중심점 옮기기
+                        float r = (Player.transform.localScale.x / 2) + 0.5f; // 반지름뒤 숫자를 수정하면됨
+                        Vector3 drag = AtkRange;
+                        Vector3 center = Player.transform.position; // 중점
 
-                        Vector3 dir = AtkRange.normalized;
+                        float theta = Mathf.Atan2(drag.y - center.y, drag.x - center.x);
+                        Vector3 edge = new Vector3(center.x + r * Mathf.Cos(theta), center.y + r * Mathf.Sin(theta));
 
-                        isRender = true;
-                        if (isRender)
+                        Vector3[] positions = new Vector3[lineRenderer.positionCount];
+                        lineRenderer.GetPositions(positions);
+                        positions[0] = edge;
+                        positions[1] = AtkRange;
+
+                        lineRenderer.enabled = true;
+                        lineRenderer.SetPositions(positions);
+
+                        #region RayCast
+                        float maxRaydistance = Vector3.Distance(edge, drag);
+                        Vector3 rayDir = (AtkRange - edge).normalized;
+                        RaycastHit2D hit = Physics2D.Raycast(edge, rayDir, maxRaydistance);
+                        if (hit == GameObject.FindGameObjectWithTag("Boss"))
                         {
-                            // 중심점 옮기기
-                            float r = (Player.transform.localScale.x / 2) + 0.5f; // 반지름뒤 숫자를 수정하면됨
-                            Vector3 drag = AtkRange;
-                            Vector3 center = Player.transform.position; // 중점
-
-                            float theta = Mathf.Atan2(drag.y - center.y, drag.x - center.x);
-                            Vector3 edge = new Vector3(center.x + r * Mathf.Cos(theta), center.y + r * Mathf.Sin(theta));
-
-                            Vector3[] positions = new Vector3[lineRenderer.positionCount];
-                            lineRenderer.GetPositions(positions);
-                            positions[0] = edge;
-                            positions[1] = AtkRange;
-
-                            lineRenderer.enabled = true;
-                            lineRenderer.SetPositions(positions);
-
-                            #region RayCast
-                            Vector3 rayDir = (AtkRange - edge).normalized;
-                            RaycastHit2D hit = Physics2D.Raycast(edge, rayDir);
-                            if (hit == GameObject.FindGameObjectWithTag("Boss"))
-                            {
-                                isRay = true;
-                            }
-                            else
-                                isRay = false;
-                            #endregion
-
+                            isRay = true;
                         }
-                    }
-                }
-                else if (FindTouchById(0).phase == TouchPhase.Ended || FindTouchById(1).phase == TouchPhase.Ended) // 손을 뗏을때1
-                {
-                    isAtk = false;
-                    isRay = false;
-                    if (isRender) // 그렸던 라인을 지우는 과정
-                    {
-                        Vector3 touchWorldPosition = Camera.main.ScreenToWorldPoint(touch2.position);
-                        Player.transform.position = new Vector2(touchWorldPosition.x, touchWorldPosition.y);
-                        isRender = false;
-                        lineRenderer.enabled = false;
-                        lineRenderer.SetPositions(new Vector3[0]);
+                        else
+                            isRay = false;
+                        #endregion
 
                     }
                 }
-
             }
 
-                Touch FindTouchById(int id)
-                {
-                    for (int i = 0; i < Input.touchCount; ++i)
-                    {
-                        Touch t = Input.GetTouch(i);
-                        if (t.fingerId == id) return t;
-                    }
+        }
 
-                    return default(Touch);
-                } // 터치 순서따라 finger id값 저장
+
+  
+        if (Input.touchCount == 0)
+        {
+            isAtk = false;
+            isRay = false;
+            if (isRender) // 그렸던 라인을 지우는 과정
+            {
+                touchWorldPosition = Camera.main.ScreenToWorldPoint(touch2prePoisition);
+                Player.transform.position = new Vector2(touchWorldPosition.x, touchWorldPosition.y);
+                isRender = false;
+                lineRenderer.enabled = false;
+                lineRenderer.SetPositions(new Vector3[0]);
+            }
+        }
+        Touch FindTouchById(int id)
+        {
+            for (int i = 0; i < Input.touchCount; ++i)
+            {
+                Touch t = Input.GetTouch(i);
+                if (t.fingerId == id) return t;
             }
 
-        if(isRay) // 몬스터가 피격중일때
+            return default(Touch);
+        } // 터치 순서따라 finger id값 저장            
+
+        if (isRay) // 몬스터가 피격중일때
         {
             TestImage.color = Color.red;
         }
-        if(!isRay) // 테스트용 나중에 지우기
+        if (!isRay) // 테스트용 나중에 지우기
         {
             TestImage.color = Color.white;
         }
     }
- 
-}
 
+}
