3e613b1d8c7d001fecdaf45c751ef5880ea4ec0c
junHyeong7083 <127365389+junHyeong7083@users.noreply.github.com>
2023-03-25 14:01:02 +0900

    testing
    약간 코드수정

--- Assets/script/Monster/Stage1_Boss.cs
+++ Assets/script/Monster/Stage1_Boss.cs
@@ -9,14 +9,15 @@ public class Stage1_Boss : MonoBehaviour
     Rigidbody2D rigidbody2D;
     private PlayerController playerController;
     public GameObject Player;
+    Vector3 PlayerPos;
     // ----------------- Pattern2 -----------------
     bool nextPtn1State = false;
     public GameObject pattern2;
     // ----------------- Pattern3 -----------------
-    public GameObject pattern3_1;
+    public GameObject pattern3;
+    GameObject pattern3_1;
     GameObject pattern3_2;
     GameObject pattern3_3;
-    float pattern3speed = 3f;
     // ----------------- bool -----------------
     bool isOverlab = false;
     void Start()
@@ -25,10 +26,6 @@ public class Stage1_Boss : MonoBehaviour
         rigidbody2D = GetComponent<Rigidbody2D>();
         playerController = Player.GetComponent<PlayerController>();
 
-        #region 오브젝트 복제
-        pattern3_2 = Instantiate(pattern3_1);
-        pattern3_3 = Instantiate(pattern3_1);
-        #endregion
     }
     void OverlabMixPattern()
     {
@@ -58,11 +55,11 @@ public class Stage1_Boss : MonoBehaviour
         }
 
         startTime = Time.time; // 시작 시간 재설정
-        while (Time.time - startTime < 2.5f) // 2.5초간 position.y 값 변경 및 rotation.z 값 변경
+        while (Time.time - startTime < 2.0f) // 2.5초간 position.y 값 변경 및 rotation.z 값 변경
         {
             if(pattern2.transform.position.y  > -2)
             {
-                pattern2.transform.position += new Vector3(1.0f * Time.deltaTime, -4 * 3 * Time.deltaTime, 0);
+                pattern2.transform.position += new Vector3(1.0f * Time.deltaTime, -4 * 4 * Time.deltaTime, 0);
                 pattern2.transform.Rotate(0, 0, -70 *  4 * Time.deltaTime);
             }
   
@@ -90,11 +87,11 @@ public class Stage1_Boss : MonoBehaviour
         }
 
         startTime = Time.time; // 시작 시간 재설정
-        while (Time.time - startTime < 2.5f) // 2.5초간 position.y 값 변경 및 rotation.z 값 변경
+        while (Time.time - startTime < 2.0f) // 2.5초간 position.y 값 변경 및 rotation.z 값 변경
         {
             if (pattern2.transform.position.y < 2)
             {
-                pattern2.transform.position += new Vector3(-1.0f * Time.deltaTime, 4 * 3 * Time.deltaTime, 0);
+                pattern2.transform.position += new Vector3(-1.0f * Time.deltaTime, 4 * 4 * Time.deltaTime, 0);
                 pattern2.transform.Rotate(0, 0, -70 * 4 * Time.deltaTime);
             }
 
@@ -108,14 +105,22 @@ public class Stage1_Boss : MonoBehaviour
 
     public void Scp1_3()
     {
+        #region 복제
+        pattern3_1 = Instantiate(pattern3.gameObject);
+        pattern3_2 = Instantiate(pattern3.gameObject);
+        pattern3_3 = Instantiate(pattern3.gameObject);
+        #endregion
+
         isOverlab = true;
         #region Pattern3 Setting Pos
-        pattern3_1.transform.position =  new Vector3(Player.transform.position.x - 0.7f , Player.transform.position.y - 1f, 0);
-        pattern3_1.transform.Rotate(0, 0, 220);
-        pattern3_2.transform.position = new Vector3(0, Player.transform.position.y- 1.3f, 0);
-        pattern3_2.transform.Rotate(0, 0, 270);
-        pattern3_3.transform.position =new Vector3(Player.transform.position.x + 0.7f, Player.transform.position.y - 1f, 0);
-        pattern3_3.transform.Rotate(0, 0, 320);
+        pattern3_1.transform.position = new Vector3(PlayerPos.x - 1.3f, PlayerPos.y - f, 0);
+        pattern3_1.transform.eulerAngles = new Vector3(0, 0, 220);
+
+        pattern3_2.transform.position = new Vector3(0, Mathf.Round(PlayerPos.y - 1.3f), 0);
+        pattern3_2.transform.eulerAngles = new Vector3(0, 0, 270);
+
+        pattern3_3.transform.position = new Vector3(PlayerPos.x + 0.7f, PlayerPos.y - 1f, 0);
+        pattern3_3.transform.eulerAngles = new Vector3(0, 0, 320);
         #endregion
         StartCoroutine(Scp1_3_1());
     }
@@ -124,23 +129,49 @@ public class Stage1_Boss : MonoBehaviour
         pattern3_1.SetActive(true);
         pattern3_2.SetActive(true);
         pattern3_3.SetActive(true);
+
+        Vector3 dir1 = (PlayerPos - pattern3_1.transform.position).normalized;
+        Vector3 dir2 = (PlayerPos - pattern3_2.transform.position).normalized;
+        Vector3 dir3 = (PlayerPos - pattern3_3.transform.position).normalized;
+
         float startTime = Time.time; // 시작 시간 저장
         while (Time.time - startTime < 2) // 1초간 rotation.z 값 변경
         {
-            float backPos = 0.1f;
-          pattern3_1.transform.position += new Vector3(-backPos * Time.deltaTime, -backPos * Time.deltaTime, 0);
-          pattern3_2.transform.position += new Vector3(0, -backPos * Time.deltaTime, 0);
-          pattern3_3.transform.position += new Vector3(backPos * Time.deltaTime, -backPos * Time.deltaTime, 0);
-                yield return null;
-            
+             float backPos = 0.1f;
+            pattern3_1.transform.position += new Vector3(-backPos * Time.deltaTime, -backPos * Time.deltaTime, 0);
+            pattern3_2.transform.position += new Vector3(0, -backPos * Time.deltaTime, 0);
+            pattern3_3.transform.position += new Vector3(backPos * Time.deltaTime, -backPos * Time.deltaTime, 0);
+
+
+            yield return null;          
         }
+        startTime = Time.time;
+        while (Time.time - startTime < 2) // 1초간 rotation.z 값 변경
+        {
+            float speed = 5f;
+            pattern3_1.transform.position += new Vector3(speed * dir1.x * Time.deltaTime, speed * dir1.y * Time.deltaTime, 0);
+            pattern3_2.transform.position += new Vector3(speed * dir2.x * Time.deltaTime, speed * dir2.y * Time.deltaTime, 0);
+            pattern3_3.transform.position += new Vector3(speed * dir3.x * Time.deltaTime, speed * dir3.y * Time.deltaTime, 0);
+
+            yield return null;
+        }
+
+        Destroy(pattern3_1);
+        Destroy(pattern3_2);
+        Destroy(pattern3_3);
+
+        StopCoroutine(Scp1_3_1());
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        if (PlayerController.atkState) // 공격상태이면
+        PlayerPos = Player.transform.position;
+        Debug.Log(PlayerPos.x);
+        Debug.Log(+PlayerPos.y);
+
+         if(PlayerController.atkState) // 공격상태이면
         {
             // 체력 감소하는로직
         }
