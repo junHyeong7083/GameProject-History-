cf33d9840bf67aa507e4a62b10a854fa53ca386c
junHyeong7083 <127365389+junHyeong7083@users.noreply.github.com>
2023-05-14 02:47:18 +0900

    Pattern4
    패턴4 기초공사

--- Assets/script/Monster/Stage2_Boss.cs
+++ Assets/script/Monster/Stage2_Boss.cs
@@ -23,7 +23,17 @@ public class Stage2_Boss : MonoBehaviour
     GameObject pattern2_1;
     GameObject pattern2_2;
 
+    // -------------- Overlab_Pattern 2 --------------
+    GameObject overlab_pattern2_1;
+    GameObject overlab_pattern2_2;
     Vector3 PivotPos = new Vector3(0, -2, 0);
+
+    // ----------------- Pattern 3 -----------------
+    GameObject pattern3_1;
+
+    // ----------------- Pattern 4 -----------------
+    GameObject pattern4_1;
+    GameObject pattern4_2;
     // ----------------- bool -----------------
     #region Bool
     bool isOverlab = false;
@@ -195,13 +205,173 @@ public class Stage2_Boss : MonoBehaviour
     }
     #endregion
 
+    public void Overlab_Scp2_2()
+    {
+        isOverlab = true;
+        StartCoroutine(Overlab_Scp2_2_Pattern());
+    }
+    #region Overlab_Scp2_2 패턴로직
+    IEnumerator Overlab_Scp2_2_Pattern()
+    {
+        #region Setting
+        overlab_pattern2_1 = PatternManager.Instance.StartPattern("Test");
+        overlab_pattern2_2 = PatternManager.Instance.StartPattern("Test");
+
+        SpriteRenderer sprite2_2 = overlab_pattern2_1.GetComponent<SpriteRenderer>();
+        SpriteRenderer sprite2_3 = overlab_pattern2_2.GetComponent<SpriteRenderer>();
+
+        UnityEngine.Color color2_2 = sprite2_2.color;
+        UnityEngine.Color color2_3 = sprite2_3.color;
+
+        color2_2.a = 0;
+        color2_3.a = 0;
+
+        sprite2_2.color = color2_2;
+        sprite2_3.color = color2_3;
+
+        overlab_pattern2_1.transform.position = new Vector3(25, 20, 0);
+        overlab_pattern2_2.transform.position = new Vector3(-25, 13, 0);
+
+        Vector3 startpattern2_1 = new Vector3(25, 20, 0);
+        Vector3 midpattern2_1 = new Vector3(-10, -90, 0);
+        Vector3 endpattern2_1 = new Vector3(-35, 75, 0);
+
+        Vector3 startpattern2_2 = new Vector3(-25, 13, 0);
+        Vector3 midpattern2_2 = new Vector3(8, -77, 0);
+        Vector3 endpattern2_2 = new Vector3(30, 75, 0);
+        float duration = 2.0f; // 이동 시간
+        float euler = 35f; // 회전각도
+        #endregion
+        float startTime = Time.time;
+        while (Time.time - startTime < 1) // 대기시간
+        {
+            float alpha = (Time.time - startTime) / 1f;
+
+            color2_2.a = alpha;
+            color2_3.a = alpha;
+
+            sprite2_2.color = color2_2;
+            sprite2_3.color = color2_3;
+
+            yield return null;
+        }
+        startTime = Time.time;
+        while (Time.time - startTime < 1f)
+        {
+            overlab_pattern2_1.transform.eulerAngles += new Vector3(0, 0, -euler * Time.deltaTime * 50);
+            overlab_pattern2_2.transform.eulerAngles += new Vector3(0, 0, euler * Time.deltaTime * 50);
+
+            yield return null;
+        }
+
+
+        startTime = Time.time;
+        while (Time.time - startTime < duration)
+        {
+            float t = (Time.time - startTime) / duration;
+            overlab_pattern2_1.transform.eulerAngles += new Vector3(0, 0, -euler * Time.deltaTime * 50);
+            overlab_pattern2_2.transform.eulerAngles += new Vector3(0, 0, euler * Time.deltaTime * 50);
+
+            Vector3 position1 = Vector3.Lerp(startpattern2_1, endpattern2_1, t) + midpattern2_1 * 4 * t * (1 - t); // 포물선 이동 경로 계산
+            overlab_pattern2_1.transform.position = position1; // 이동
+
+            Vector3 position2 = Vector3.Lerp(startpattern2_2, endpattern2_2, t) + midpattern2_2 * 4 * t * (1 - t); // 포물선 이동 경로 계산
+            overlab_pattern2_2.transform.position = position2; // 이동
+
+            yield return null;
+        }
+
+        startTime = Time.time;
+        while (Time.time - startTime < 3 - duration)
+        {
+            overlab_pattern2_1.transform.eulerAngles += new Vector3(0, 0, -euler * Time.deltaTime * 50);
+            overlab_pattern2_2.transform.eulerAngles += new Vector3(0, 0, euler * Time.deltaTime * 50);
+
+            yield return null;
+        }
+        Destroy(overlab_pattern2_1);
+        Destroy(overlab_pattern2_2);
+
+        isOverlab = false;
+    }
+    #endregion
+
+
+    #region 유기
     public void Scp2_3()
     {
         isPattern = true;
     }
     IEnumerator Scp2_3_Pattern()
     {
+        float startTime = Time.time;
+        while(Time.time - startTime < 2f)
+        {
+            // 휘파람 사운드
+            yield return null;
+        }
         yield return null;
+    }
+    #endregion
+    public void Scp2_4()
+    {
+        isPattern = true;   
+    }
+    IEnumerator Scp2_4_Pattern()
+    {
+        pattern4_1 = PatternManager.Instance.StartPattern("Test");
+        pattern4_2 = PatternManager.Instance.StartPattern("Test");
+        #region Setting
+        SpriteRenderer sprite4_1 = pattern4_1.GetComponent<SpriteRenderer>();
+        SpriteRenderer sprite4_2 = pattern4_2.GetComponent<SpriteRenderer>();
+
+        UnityEngine.Color color4_1 = sprite4_1.color;
+        UnityEngine.Color color4_2 = sprite4_2.color;
+
+        color4_1.a = 0f;
+        color4_2.a = 0f;
+
+        sprite4_1.color= color4_1;
+        sprite4_2.color= color4_2;
+        #endregion
+        float startTime = Time.time;
+        while (Time.time - startTime < 1f)
+        {
+            // 투명도 점점 올리기
+            float alpha = (Time.time - startTime) / 1f;
+            color4_1.a = alpha;
+            color4_2.a = alpha;
+
+            sprite4_1.color = color4_1;
+            sprite4_2.color = color4_2;
+
+            yield return null;
+        }
+        startTime = Time.time;
+        while (Time.time - startTime < 2.0f)
+        {
+            // 날라갔다
+            yield return null;
+        }
+        startTime = Time.time;
+        while(Time.time - startTime < 2.0f)
+        {
+            // 돌아오기
+            yield return null;
+        }
+        startTime = Time.time;
+        while(Time.time - startTime < 1f)
+        {
+            // 투명도 점점0
+            float alpha = (Time.time - startTime) / 1f;
+            color4_1.a = 1- alpha;
+            color4_2.a = 1 - alpha;
+
+            sprite4_1.color = color4_1;
+            sprite4_2.color = color4_2;
+
+            yield return null;
+        }
     }
 
     private void Update()
